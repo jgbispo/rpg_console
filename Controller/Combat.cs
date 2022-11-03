67162307b42a182dd24d3888818daba9a70b4412
bshop <jgbispo20@gmail.com>
2022-11-03 10:12:41 -0300

    New System
    New class-based combat system

--- Controller/Combat.cs
+++ Controller/Combat.cs
@@ -3,14 +3,29 @@ namespace RPG
 
   class Combat
   {
-    public static bool Battle(int dice, Player player, Enemy enemy, bool isBattle)
+    public static bool Battle(int dice, Player player, string attack, Enemy enemy, bool isBattle)
     {
       Console.Clear();
+      int damage = 0;
+      if (attack == "sword")
+      {
+        damage = player.Strength;
+      }
+      else if (attack == "magic")
+      {
+        damage = player.Magic;
+        player.Mana -= 10;
+      }
+      else
+      {
+        damage = (player.Strength + player.Magic) / 2;
+      }
+
       if (dice == 20)
       {
         Console.WriteLine("You critical hit the enemy!");
-        Console.WriteLine("You did " + player!.Strength * 2 + " damage!");
-        if (enemy.Damage(player.Strength * 2))
+        Console.WriteLine("You did " + damage * 2 + " damage!");
+        if (enemy.Damage(damage * 2))
         {
           player.Gold += enemy.DropGold();
           player.Experience += enemy.DropXP();
@@ -28,8 +43,8 @@ namespace RPG
       else if (dice >= 10)
       {
         Console.WriteLine("You hit the enemy!");
-        Console.WriteLine("You did " + player!.Strength + " damage!");
-        if (enemy.Damage(player.Strength))
+        Console.WriteLine("You did " + damage + " damage!");
+        if (enemy.Damage(damage))
         {
           player.Gold += enemy.DropGold();
           player.Experience += enemy.DropXP();
@@ -62,6 +77,69 @@ namespace RPG
         Console.WriteLine("You took " + enemy.Strength + " damage!");
         player.Damage(enemy.Strength);
         Console.WriteLine("You got " + player.Health + " health!");
+      }
+      return isBattle;
+    }
+
+    public static bool UsePotionHealth(int dice, bool isBattle, Enemy enemy, Player player)
+    {
+      Console.Clear();
+      if (dice >= 10)
+      {
+        Console.WriteLine("You used a potion!");
+        if (player.Health <= player.MaxHealth)
+        {
+          player.Health += (player.MaxHealth * 10) / 100;
+          if (player.Health > player.MaxHealth)
+          {
+            player.Health = player.MaxHealth;
+          }
+          Console.WriteLine("You got " + player.Health + " health!");
+        }
+        else
+        {
+          Console.WriteLine("You have a full life!");
+          Console.WriteLine("You got " + player.Health + " health!");
+        }
+      }
+      else
+      {
+        Console.WriteLine("You couldn't use a potion!");
+        Console.WriteLine("You took " + enemy.Strength + " damage!");
+        player.Damage(enemy.Strength);
+        Console.WriteLine("You got " + player.Health + " health!");
+      }
+      return isBattle;
+    }
+
+    public static bool UsePotionMana(int dice, bool isBattle, Enemy enemy, Player player)
+    {
+      Console.Clear();
+      if (dice >= 10)
+      {
+        Console.WriteLine("You used a potion!");
+        if (player.Mana <= player.MaxMana)
+        {
+          player.Mana += (player.MaxMana * 10) / 100;
+          if (player.Mana > player.MaxMana)
+          {
+            player.Mana = player.MaxMana;
+          }
+          Console.WriteLine("You got " + player.Mana + " mana!");
+        }
+        else
+        {
+          Console.WriteLine("You have a full mana!");
+          Console.WriteLine("You got " + player.Mana + " mana!");
+
+        }
+      }
+      else
+      {
+        Console.WriteLine("You couldn't use a potion!");
+        Console.WriteLine("You took " + enemy.Strength + " damage!");
+        player.Damage(enemy.Strength);
+        Console.WriteLine("You got " + player.Mana + " mana!");
       }
       return isBattle;
     }
